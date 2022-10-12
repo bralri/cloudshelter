2a4d3f3bd5ecd1b459472bdbe0ed5071f87ff39c
Bryan Ridpath <bryanridpath@gmail.com>
2022-10-12 16:35:43 +0100

    12/10/2022

--- path_test/index.js
+++ path_test/index.js
@@ -1,5 +1,4 @@
 let camera, scene, renderer, controls;
-let camera_location_picker;
 
 let moveForward = false;
 let moveBackward = false;
@@ -11,55 +10,42 @@ let prevTime = performance.now();
 const velocity = new THREE.Vector3();
 const direction = new THREE.Vector3();
 
-let alexVideo, alexVideoTexture, alexSound;
-let christophVideo, christophTexture, christophScreen, christophSound;
-
-let degreeX = 0;
-let degreeY = 0;
-let degreeZ = 0;
-
-let manager;
-
-//
+let video, videoScreen, videoTexture, sound;
 
 const up = new THREE.Vector3(1, 0, 0);
 const axis = new THREE.Vector3();
 let fraction = 0;
-let path_1, path_2;
-let point_1, point_2;
 
-//
+let degreeX = 0; 
+let degreeY = 0; 
+let degreeZ = 0;
 
-const white = new THREE.Color(0xffffff);
-white.convertSRGBToLinear();
-const midGrey = new THREE.Color(0x63666A);
-midGrey.convertSRGBToLinear();
-const black = new THREE.Color(0x000000);
-black.convertSRGBToLinear();
+let manager;
 
-//
+let pointsArray = [];
+let objIntersects = [];
+let videos_ = [];
+let videoplay_ = [];
+let soundplay_ = [];
+let playing = false;
 
 init();
 animate();
 
-//
-
 function init() {
     sceneSetup();
     controlsSetup();
-
     loadingManager();
-
     drawPath();
     loadModels();
-    load_Sounds_Videos();
+    load_Videos();
     load_Stills();
 }
 
 function sceneSetup() {
     scene = new THREE.Scene();
-    scene.background = black;
-    scene.fog = new THREE.Fog(black, 0, 1800);
+    scene.background = 0x000000;
+    scene.fog = new THREE.Fog(0x000000, 0, 2000);
 
     camera = new THREE.PerspectiveCamera( 
         75, 
@@ -73,7 +59,7 @@ function sceneSetup() {
     const ambLight = new THREE.AmbientLight(0x404040, 2);
     scene.add(ambLight);
 
-    const light = new THREE.HemisphereLight(midGrey, black, 0.5);
+    const light = new THREE.HemisphereLight(0x63666A, 0x000000, 0.5);
     light.position.set(0, 10, 0);
     scene.add(light);
 
@@ -125,7 +111,7 @@ function controlsSetup() {
         [-68, -2203]
     ]
 
-    camera_location_picker = Math.floor(Math.random() * camera_location_array.length);
+    let camera_location_picker = Math.floor(Math.random() * camera_location_array.length);
 
     controls.getObject().position.x = camera_location_array[camera_location_picker][0];
     controls.getObject().position.z = camera_location_array[camera_location_picker][1];
@@ -208,7 +194,7 @@ function rotateObject(object, degreeX, degreeY, degreeZ) {
 }
 
 function drawPath() {
-    const path = [
+    pointsArray = [
         [
             {
                 line: new THREE.CatmullRomCurve3([
@@ -219,7 +205,7 @@ function drawPath() {
                         new THREE.Vector3(290, 8, -1359),
                         new THREE.Vector3(393, 8, 187),
                         new THREE.Vector3(100, 8, 218)
-                    ], true, "centripetal"),
+                ], true, "centripetal")
             },
             {
                 line: new THREE.CatmullRomCurve3([
@@ -240,19 +226,19 @@ function drawPath() {
                         new THREE.Vector3(-306, 8, -1551),
                         new THREE.Vector3(-357, 8, -1815),
                         new THREE.Vector3(-416, 8, -1919)
-                    ], true, "centripetal")
+                ], true, "centripetal") 
             }
         ]
     ]
-
-    for (let i = 0; i < path[0][i].length; i++) {
-        const paths = path[0][i];
-        let vertices = paths.getSpacedPoints(100);
+    
+    for (let i = 0; i < pointsArray[0].length; i++) {
+        let points = pointsArray[0][i].line;
+        let vertices = points.getSpacedPoints(100);
 
         const lineGeometry = new THREE.BufferGeometry().setFromPoints(vertices);
         const lineMaterial = new THREE.LineBasicMaterial({
             color: 0xffffff,
-            visible: true,
+            visible: false,
         });
         const line = new THREE.Line(lineGeometry, lineMaterial);
         scene.add(line);
@@ -368,312 +354,187 @@ function loadModels() {
     }
 }
 
-function load_Sounds_Videos() {
+function load_Videos() {
     const videos = [
         [
             {
-                ID: "Picnic",
-                soundURL: "../sound/voidshow/Alex/Picnic.mp3",
+                ID: "Elf",
+                artist: "Alex",
+                title: "Corpse",
+                date: "2022",
+                info: "",
+                MP3: "../sound/voidshow/Alex/Elf.mp3",
                 geometry: new THREE.PlaneBufferGeometry(10, 20),
-                rx: 0,
-                ry: Math.PI / 2,
-                rz: 0,
-                side: THREE.DoubleSide,
-                transparent: true,
-                opacity: 1,
-                refDistance: 2,
-                volume: 0.5,
-                coneInnerAngle: 360,
-                coneOuterAngle: 360,
-                coneOuterGain: 0
+                transparency: true,
+                innercone: 360,
+                outercone: 360,
+                rotation: Math.PI/2
             },
             {
-                ID: "Elf",
-                soundURL: "../sound/voidshow/Alex/Elf.mp3",
+                ID: "Picnic",
+                artist: "Alex",
+                title: "Corpse",
+                date: "2022",
+                info: "",
+                MP3: "../sound/voidshow/Alex/Picnic.mp3",
                 geometry: new THREE.PlaneBufferGeometry(10, 20),
-                rx: 0,
-                ry: Math.PI / 2,
-                rz: 0,
-                side: THREE.DoubleSide,
-                transparent: true,
-                opacity: 1,
-                refDistance: 2,
-                volume: 0.1,
-                coneInnerAngle: 360,
-                coneOuterAngle: 360,
-                coneOuterGain: 0
+                transparency: true,
+                innercone: 360,
+                outercone: 360,
+                rotation: Math.PI/2
+            },
+            {
+                ID: "Christoph",
+                artist: "Christoph",
+                title: "The Void, Suicide and The Sorrowing of Man",
+                date: "2022",
+                info: "11 minute loop",
+                MP3: "../sound/voidshow/Christoph/christoph.mp3",
+                geometry: new THREE.BoxBufferGeometry(70, 45, 2),
+                transparency: false,
+                innercone: 180,
+                outercone: 230,
+                rotation: -62,
+                px: 1093,
+                py: 30,
+                pz: -2110
             }
         ]
     ]
 
-    const audioListener = new THREE.AudioListener();
-    camera.add(audioListener);
-
     for (let i = 0; i < videos[0].length; i++) {
         const object = videos[0][i];
-        let videoID = document.getElementById(object.ID);
-        alexVideoTexture = new THREE.VideoTexture(videoID);
-        alexVideoTexture.encoding = THREE.sRGBEncoding;
-        alexVideoTexture.minFilter = THREE.LinearFilter;
-        alexVideoTexture.magFilter = THREE.LinearFilter;
+        let video = document.getElementById(object.ID);
+        videoTexture = new THREE.VideoTexture(video);
+        videoTexture.encoding = THREE.sRGBEncoding;
+        videoTexture.minFilter = THREE.LinearFilter;
+        videoTexture.magFilter = THREE.LinearFilter;
         let videoGeometry = object.geometry;
-        rotateObject(videoGeometry, object.rx, object.ry, object.rz);
-        let videoMaterial = new THREE.MeshBasicMaterial({
-            map: alexVideoTexture,
-            side: object.side,
-            transparent: object.transparent,
-            opacity: object.opacity,
-        });
-        alexVideo = new THREE.Mesh(videoGeometry, videoMaterial);
-        alexVideo.receiveShadow = false;
-        alexVideo.castShadow = false;
-        scene.add(alexVideo);
-        // alexVideo.play();
-        alexSound = new THREE.PositionalAudio(audioListener);
+        let videoMaterial = new THREE.MeshLambertMaterial({
+                map: videoTexture,
+                side: THREE.DoubleSide,
+                transparent: object.transparency,
+                opacity: 1
+            });
+        videoScreen = new THREE.Mesh(videoGeometry, videoMaterial);
+        if (object.artist == "Christoph") {
+            videoScreen.position.set(object.px, object.py, object.pz);
+            rotateObject(videoScreen, 0, object.rotation, 0);
+        } else if (object.artist == "Alex") {
+            rotateObject(videoScreen, 0, object.rotation, 0);
+        }
+        videoScreen.receiveShadow = false;
+        videoScreen.castShadow = false;
+        scene.add(videoScreen);
+
+        const audioListener = new THREE.AudioListener();
+        camera.add(audioListener);
+        sound = new THREE.PositionalAudio(audioListener);
         const audioLoader = new THREE.AudioLoader(manager);
-        audioLoader.load(object.soundURL, function(buffer) {
-            alexSound.setBuffer(buffer);
-            alexSound.setLoop(true);
-            alexSound.setRefDistance(object.refDistance);
-            alexSound.setVolume(object.volume);
-            alexSound.setDirectionalCone(
-                object.coneInnerAngle, 
-                object.coneOuterAngle, 
-                object.coneOuterGain
-            );
+        audioLoader.load(object.MP3, function(buffer) {
+            sound.setBuffer(buffer);
+            sound.setLoop(true);
+            sound.setRefDistance(4);
+            sound.setVolume(1);
+            sound.setDirectionalCone(object.innercone, object.outercone, 0.1);
         })
-        alexVideo.add(alexSound);
+
+        videoplay_.push(video);
+        soundplay_.push(sound);
+
+        videoScreen.add(sound);
+        videos_.push(videoScreen);
     }
-
-    // const picnic = {
-    //     ID: "Picnic",
-    //     Sound: "../sound/voidshow/Alex/Picnic.mp3",
-    // };
-
-    // //Picnic.mov
-    // picnicVideo = document.getElementById(picnic.ID);
-    // picnicTexture = new THREE.VideoTexture(picnicVideo);
-    // picnicTexture.encoding = THREE.sRGBEncoding;
-    // picnicTexture.minFilter = THREE.LinearFilter;
-    // picnicTexture.magFilter = THREE.LinearFilter;
-    // picnicTexture.format = THREE.RGBAFormat;
-    // let picnicGeometry = new THREE.PlaneBufferGeometry(10, 20);
-    // picnicGeometry.rotateY(Math.PI / 2);
-    // const picnicMaterials = new THREE.MeshBasicMaterial({
-    //     map: picnicTexture,
-    //     side: THREE.DoubleSide,
-    //     transparent: true,
-    //     opacity: 1
-    // });
-    // picnicPlaneScreen = new THREE.Mesh(picnicGeometry, picnicMaterials);
-    // picnicPlaneScreen.receiveShadow = false;
-    // picnicPlaneScreen.castShadow = false;
-    // scene.add(picnicPlaneScreen);
-    // //Picnic.mp3
-    // picnicSound = new THREE.PositionalAudio(audioListener);
-    // const picnicAudioLoader = new THREE.AudioLoader(manager);
-    // picnicAudioLoader.load(picnic.Sound, function(buffer) {
-    //     picnicSound.setBuffer(buffer);
-    //     picnicSound.setLoop(true);
-    //     picnicSound.setRefDistance(2);
-    //     picnicSound.setVolume(0.5);
-    //     picnicSound.setDirectionalCone(360, 360, 0);
-    // })
-    // picnicPlaneScreen.add(picnicSound);
-
-    // //Elf.mov
-    // elfVideo = document.getElementById("Elf");
-    // elfTexture = new THREE.VideoTexture(elfVideo);
-    // elfTexture.encoding = THREE.sRGBEncoding;
-    // elfTexture.minFilter = THREE.LinearFilter;
-    // elfTexture.magFilter = THREE.LinearFilter;
-    // elfTexture.format = THREE.RGBAFormat;
-    // let elfGeometry = new THREE.PlaneBufferGeometry(10, 20);
-    // elfGeometry.rotateY(Math.PI / 2);
-    // const elfMaterials = new THREE.MeshBasicMaterial({
-    //     map: elfTexture,
-    //     side: THREE.DoubleSide,
-    //     transparent: true,
-    //     opacity: 1
-    // });
-    // elfPlaneScreen = new THREE.Mesh(elfGeometry, elfMaterials);
-    // elfPlaneScreen.receiveShadow = false;
-    // elfPlaneScreen.castShadow = false;
-    // scene.add(elfPlaneScreen);
-    // //Elf.mp3
-    // elfSound = new THREE.PositionalAudio(audioListener);
-    // const elfAudioLoader = new THREE.AudioLoader(manager);
-    // elfAudioLoader.load('../sound/voidshow/Alex/Elf.mp3', function(buffer) {
-    //     elfSound.setBuffer(buffer);
-    //     elfSound.setLoop(true);
-    //     elfSound.setRefDistance(2);
-    //     elfSound.setVolume(0.1);
-    //     elfSound.setDirectionalCone(360, 360, 0)
-    // })
-    // elfPlaneScreen.add(elfSound);
-
-    //Christoph.mp4
-    christophVideo = document.getElementById("Christoph");
-    christophTexture = new THREE.VideoTexture(christophVideo);
-    christophTexture.encoding = THREE.sRGBEncoding;
-    christophTexture.minFilter = THREE.LinearFilter;
-    christophTexture.magFilter = THREE.LinearFilter;
-    let christophGeometry = new THREE.BoxGeometry(70, 45, 1);
-    let christopMaterials = [
-        new THREE.MeshBasicMaterial({
-            color: black
-        }),
-        new THREE.MeshBasicMaterial({
-            color: black
-        }),
-        new THREE.MeshBasicMaterial({
-            color: black
-        }),
-        new THREE.MeshBasicMaterial({
-            color: black
-        }),
-        new THREE.MeshBasicMaterial({
-            map: christophTexture,
-            side: THREE.DoubleSide,
-            transparent: false,
-            opacity: 1
-        }),
-        new THREE.MeshBasicMaterial({
-            color: black,
-            side: THREE.BackSide
-        })
-    ];
-    christophScreen = new THREE.Mesh(christophGeometry, christopMaterials);
-    christophScreen.position.set(1093, 30, -2110);
-    rotateObject(christophScreen, 0, -62, 0);
-    scene.add(christophScreen);
-    //christoph.mp3
-    christophSound = new THREE.PositionalAudio(audioListener);
-    const christophAudioLoader = new THREE.AudioLoader(manager);
-    christophAudioLoader.load('../sound/voidshow/Christoph/christoph.mp3', function(buffer) {
-        christophSound.setBuffer(buffer);
-        christophSound.setLoop(true);
-        christophSound.setRefDistance(2);
-        christophSound.setVolume(0.5);
-        christophSound.setDirectionalCone(180, 230, 0.1)
-    })
-    christophScreen.add(christophSound);
 }
 
 function load_Stills() {
     const imgs = [
         [
             {
-                color: 0xffffff,
-                geometry: new THREE.BoxBufferGeometry(2, 20, 20),
+                img: "../img/voidshow/about/Alex.png",
                 px: 961,
                 py: 10,
-                pz: -2021,
-                rotation: -30
+                pz: -2021
             },
             {
-                color: 0x000000,
-                geometry: new THREE.BoxBufferGeometry(2, 20, 20),
+                img: "../img/voidshow/about/Brian.png",
                 px: 980,
                 py: 10,
-                pz: -2055,
-                rotation: -30
+                pz: -2055
             },
             {
-                color: 0x0000ff,
-                geometry: new THREE.BoxBufferGeometry(2, 20, 20),
+                img: "../img/voidshow/about/Bryan.png",
                 px: 999,
                 py: 10,
-                pz: -2088,
-                rotation: -30
+                pz: -2088
             },
             {
-                color: 0x00ff00,
-                geometry: new THREE.BoxBufferGeometry(2, 20, 20),
+                img: "../img/voidshow/about/Christoph.png",
                 px: 1008,
                 py: 10,
-                pz: -1935,
-                rotation: -30
+                pz: -1935
             },
             {
-                color: 0xff0000,
-                geometry: new THREE.BoxBufferGeometry(2, 20, 20),
+                img: "../img/voidshow/about/Gwen.png",
                 px: 1031,
                 py: 10,
-                pz: -1976,
-                rotation: -30
+                pz: -1976
             },
             {
-                color: 0xffff00,
-                geometry: new THREE.BoxBufferGeometry(2, 20, 20),
+                img: "../img/voidshow/about/Molly.png",
                 px: 1049,
                 py: 10,
-                pz: -2009,
-                rotation: -30
-            },
+                pz: -2009
+            }
         ]
     ];
 
     for (let i = 0; i < imgs[0].length; i++) {
         const object = imgs[0][i];
-        const geometry = object.geometry;
-        const material = new THREE.MeshPhongMaterial({
-            color: object.color
+        const geometry = new THREE.BoxBufferGeometry(2, 20, 20);
+        let texture = new THREE.TextureLoader(manager).load(
+            object.img
+        );
+        const material = new THREE.MeshLambertMaterial({
+            map: texture,
+            side: THREE.DoubleSide
         });
         const cube = new THREE.Mesh(geometry, material);
         cube.position.set(object.px, object.py, object.pz);
-        rotateObject(cube, 0, object.rotation, 0);
+        rotateObject(cube, 0, -30, 0);
         
         scene.add(cube);
     };
 }
 
 function videoPlaneMove() {
-    // //picnicPlaneScreen
-    // const picnicPosition = path_2.getPoint(fraction);
-    // const picnicTangent = path_2.getTangent(fraction);
-    // picnicPlaneScreen.position.copy(picnicPosition);
-    // axis.crossVectors(up, picnicTangent).normalize();
-    // const picnicRadians = Math.acos(up.dot(picnicTangent));
-    // picnicPlaneScreen.quaternion.setFromAxisAngle(axis, picnicRadians);
+    for (let i = 0; i < 2; i++) {
+        const path = pointsArray[0][i].line;
+        const position = path.getPoint(fraction);
+        const tangent = path.getTangent(fraction);
+        const corpseVideo = videos_[i];
+        corpseVideo.position.copy(position);
+        axis.crossVectors(up, tangent).normalize();
+        const radians = Math.acos(up.dot(tangent));
+        corpseVideo.quaternion.setFromAxisAngle(axis, radians);
+    }
 
-    // //elfPlaneScreen
-    // const elfPosition = path_1.getPoint(fraction);
-    // const elfTangent = path_1.getTangent(fraction);
-    // elfPlaneScreen.position.copy(elfPosition);
-    // axis.crossVectors(up, elfTangent).normalize();
-    // const elfRadians = Math.acos(up.dot(elfTangent));
-    // elfPlaneScreen.quaternion.setFromAxisAngle(axis, elfRadians);
-    // for (let i = 0; i < path[0].length; i++) {
-    //     const position = path[i].getPoint(fraction);
-    //     const tangent = path[i].getTangent(fraction);
-    //     alexVideo.position.copy(position);
-    //     axis.crossVectors(up, tangent).normalize();
-    //     const radians = Math.acos(up.dot(tangent));
-    //     alexVideo.quaternion.setFromAxisAngle(axis, radians);
-    // }
-
-    // fraction += 0.00001;
-    // if (fraction > 1) {
-    //     fraction = 0;
-    // }
+    fraction += 0.00001;
+    if (fraction > 1) {
+        fraction = 0;
+    }
 }
 
 function play_Sounds_Videos() {
-    //Picnic
-    // picnicVideo.play();
-    // picnicSound.play();
-
-    // //Elf
-    // elfVideo.play();
-    // elfSound.play();
-
-    // alexVideo.play();
-    alexSound.play();
-
-    //Christoph
-    christophVideo.play();
-    christophSound.play();
+    if (!playing) {
+        for (let i = 0; i < soundplay_.length; i++) {
+            soundplay_[i].play();
+        }
+        for (let i = 0; i < videoplay_.length; i++) {
+            videoplay_[i].play();
+        }
+    }
+    playing = true;
 }
 
 function onWindowResize() {
@@ -686,8 +547,9 @@ function onWindowResize() {
 function render() {
     renderer.render(scene, camera);
 
-    alexVideoTexture.needsUpdate = true;
-    christophTexture.needsUpdate = true;
+    if (videoTexture) {
+        videoTexture.needsUpdate = true;
+    }
 }
 
 function animate() {
